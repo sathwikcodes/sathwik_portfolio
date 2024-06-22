e0b0c2fa2333a1d9721503e6d8e0152ff13dc20c
Sathwik Batta <bnvsathwik75@gmail.com>
2024-06-22 12:59:06 +0530

    resume

--- components/intro.tsx
+++ components/intro.tsx
@@ -9,7 +9,7 @@ import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
 import { useActiveSectionContext } from "@/context/active-section-context";
-import profile from "@/public/Sathwik.png";
+import profile from "@/public/Sathwik.jpg";
 
 export default function Intro() {
   const { ref } = useSectionInView("Home", 0.5);
